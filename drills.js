a5fc33a661e9aa7e0d972db55c8c949316dd1fb2
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-01-29 18:54:27 -0800

    finished last of required drills

--- drills.js
+++ drills.js
@@ -1,3 +1,5 @@
+const Bst = require('./bst');
+const Queue = require('./Queue');
 function main() {
   let arr = [
     3,
@@ -12,3 +14,272 @@ function main() {
     18
   ];
 }
+
+const library = [
+  {
+    author: 'Cowlishaw, Mike',
+    dewey: '005.133',
+    title: 'The REXX Language'
+  },
+  {
+    author: 'Sams',
+    dewey: '005.133',
+    title:
+      'Teach Yourself C++ In 21 Days'
+  },
+  {
+    author: 'Stroustrup., Bjarne',
+    dewey: '005.133',
+    title:
+      'The C++ Programming Language'
+  },
+  {
+    author: 'Crockford, Douglas',
+    dewey: '005.2762',
+    title: 'JavaScript: The Good Parts'
+  },
+  {
+    author: 'Flanagan, David',
+    dewey: '005.2762',
+    title:
+      'JavaScript: The Definitive Guide'
+  },
+  {
+    author: 'Schmidt, Meinhard',
+    dewey: '005.44684',
+    title: 'Windows Vista for Dummies'
+  },
+  {
+    author: 'Zondervan',
+    dewey: '220.52081',
+    title: 'NIV Study Bible'
+  },
+  {
+    author: 'Humphries, Russell, Dr.',
+    dewey: '231.7652',
+    title: 'Starlight and Time'
+  },
+  {
+    author: 'Jane, Frederick Thomas',
+    dewey: '623.82509051',
+    title: "Jane's Fighting Ships"
+  },
+  {
+    author: 'Norris, Chuck',
+    dewey: '796.8092',
+    title:
+      'The Official Chuck Norris Fact Book'
+  }
+];
+function librarySearch(
+  data,
+  title,
+  dewey,
+  start = 0,
+  end = data.length - 1
+) {
+  if (start > end) {
+    return 'could not be found';
+  }
+
+  let mid = Math.floor(
+    (start + end) / 2
+  );
+
+  if (data[mid].dewey === dewey) {
+    if (data[mid].title === title) {
+      console.log(data[mid]);
+      return data[mid];
+    } else if (
+      data[mid.title] < title
+    ) {
+      librarySearch(
+        data,
+        title,
+        dewey,
+        ++mid,
+        ++mid
+      );
+    } else {
+      librarySearch(
+        data,
+        title,
+        dewey,
+        --mid,
+        --mid
+      );
+    }
+  }
+  let lessthan =
+    data[mid].dewey < dewey;
+  librarySearch(
+    data,
+    title,
+    dewey,
+    lessthan ? mid + 1 : start,
+    lessthan ? end : mid - 1
+  );
+}
+
+librarySearch(
+  library,
+  'The Official Chuck Norris Fact Book',
+  '796.8092'
+);
+
+let data = '25 15 50 10 24 35 70 4 12 18 31 44 66 90 22'
+  .split(' ')
+  .map(x => Number(x));
+
+let databst = new Bst();
+for (let datum of data) {
+  databst.insert(datum, datum);
+}
+
+function treeTransversalOne(
+  data,
+  transversals = {
+    pre: [],
+    in: [],
+    post: []
+  }
+) {
+  transversals.pre.push(data.key);
+  if (data.left) {
+    transversals = treeTransversalOne(
+      data.left,
+      transversals
+    );
+  }
+  transversals.in.push(data.key);
+  if (data.right) {
+    transversals = treeTransversalOne(
+      data.right,
+      transversals
+    );
+  }
+  transversals.post.push(data.key);
+  return transversals;
+}
+
+console.log(
+  treeTransversalOne(databst)
+);
+
+let orgstruct = [
+  {
+    name: 'Captain Piccard',
+    rank: 5
+  },
+  {
+    name: 'Commander Riker',
+    rank: 3
+  },
+  {
+    name: 'Commander Data',
+    rank: 6
+  },
+  {
+    name: 'Lt. Cmdr. Crusher',
+    rank: 8
+  },
+  {
+    name: 'Lt. Cmdr. Worf',
+    rank: 2
+  },
+  {
+    name: 'Lieutenant Security-officer',
+    rank: 1
+  },
+  {
+    name: 'Lt. Cmdr. LaForge',
+    rank: 4
+  },
+  {
+    name: 'Liutenant Selar',
+    rank: 7
+  }
+];
+let trekky = new Bst();
+for (let person of orgstruct) {
+  trekky.insert(
+    person.rank,
+    person.name
+  );
+}
+let commanderqueue = new Queue();
+function nextOfficer(
+  tree,
+  queue,
+  values = []
+) {
+  queue.enqueue(tree);
+  while (queue.last) {
+    let node = queue.dequeue();
+    values.push(node.value);
+    if (node.left) {
+      queue.enqueue(node.left);
+    }
+    if (node.right) {
+      queue.enqueue(node.right);
+    }
+  }
+
+  return values;
+}
+
+console.log(
+  trekky,
+  nextOfficer(trekky, commanderqueue)
+);
+
+function bigProfit() {
+  let daily = [
+    128,
+    97,
+    121,
+    123,
+    98,
+    97,
+    105
+  ];
+
+  let daytrade = new Bst();
+  let record;
+  for (
+    let day = 0;
+    day < daily.length - 2;
+    day++
+  ) {
+    record = {
+      profit:
+        daily[day + 1] - daily[day],
+      'day of the week': `${day}-${day +
+        1}`
+    };
+    daytrade.insert(
+      record.profit,
+      record
+    );
+  }
+
+  function treeTransversal(
+    data,
+    transversals = []
+  ) {
+    if (!data.right) {
+      transversals.push(data.value);
+    }
+    if (data.right) {
+      transversals = treeTransversal(
+        data.right,
+        transversals
+      );
+    }
+    return transversals;
+  }
+  console.log(
+    treeTransversal(daytrade)
+  );
+}
+bigProfit();
--- search-algorithms/src/App.js
+++ search-algorithms/src/App.js
@@ -16,6 +16,11 @@ class App extends Component {
   dataset = `89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5`.split(
     ' '
   );
+  sortedset = this.dataset
+    .sort((a, b) => {
+      return Number(a) - Number(b);
+    })
+    .map(x => Number(x));
 
   linearSearch = (data, value) => {
     this.setState({ searches: 0 });
@@ -34,7 +39,38 @@ class App extends Component {
     return "didn't find it";
   };
 
-  binarySearch = () => {};
+  binarySearch = (
+    data,
+    value,
+    start = 0,
+    end = data.length - 1,
+    count = 1
+  ) => {
+    let mid = Math.floor(
+      (start + end) / 2
+    );
+    console.log(data[mid]);
+    if (value === data[mid]) {
+      this.setState({
+        ...this.state,
+        searches: count
+      });
+      return 'found it!';
+    }
+    start =
+      value > data[mid]
+        ? mid + 1
+        : start;
+    end =
+      value > data[mid] ? end : mid - 1;
+    this.binarySearch(
+      data,
+      value,
+      start,
+      end,
+      ++count
+    );
+  };
 
   render() {
     return (
@@ -62,20 +98,32 @@ class App extends Component {
                 this.dataset,
                 this.state.number
               );
-
-              // this.setState({
-              //   ...this.state,
-              //   result: this.linearSearch(
-              //     this.dataset,
-              //     Number(
-              //       this.state.number
-              //     )
-              //   )
-              // });
             }}
           >
             {' '}
             perform linear search
+          </button>
+          <button
+            onClick={e => {
+              e.preventDefault();
+              console.log(
+                this.sortedset
+              );
+              this.setState({
+                ...this.state,
+                searches: 0
+              });
+
+              this.binarySearch(
+                this.sortedset,
+                Number(
+                  this.state.number
+                )
+              );
+            }}
+          >
+            {' '}
+            perform binary search
           </button>
         </form>
       </div>
